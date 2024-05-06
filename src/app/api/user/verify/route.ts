37b2e8dcc3da0893c7ccb5f6a18078a943a84ea7
Destiny Aigbe <75917333+Destiny-01@users.noreply.github.com>
2024-05-06 23:10:35 +0100

    Update route.ts

--- src/app/api/user/verify/route.ts
+++ src/app/api/user/verify/route.ts
@@ -3,8 +3,10 @@ import { NextRequest } from "next/server";
 
 export const GET = async (req: NextRequest, res: Response) => {
   try {
-    const { email, email_verification } = req.nextUrl.searchParams;
-    
+    const { searchParams } = req.nextUrl;
+    const email = searchParams.get("email");
+    const email_verification = searchParams.get("email_verification");
+
     const user = await User.findOne({ email, email_verification });
     if (!user) {
       return new Response("Invalid or expired verification", {
